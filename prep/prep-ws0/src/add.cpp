87e1e37cdfa04181774b6b5706ee617a7be8296c
azanmian676-lab <azanmian676@gmail.com>
2026-03-31 23:42:52 +0500

    Delete prep directory

--- prep/prep-ws0/src/add.cpp
+++
@@ -1,6 +0,0 @@
-#include "add.h"
-
-// TODO: Implement the add function
-int add(int a, int b) {
-    return 0; // Placeholder 
-}
--- prep/prep-ws0/src/add.h
+++
@@ -1,7 +0,0 @@
-#ifndef ADD_H
-#define ADD_H
-
-// Function to add two integers
-int add(int a, int b);
-
-#endif
